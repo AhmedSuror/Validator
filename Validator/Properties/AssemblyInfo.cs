be7826f2cb29197f8dcc86923beffb01b7249cfb
Ahmed H. Suror <ahmedsuror@live.com>
2021-04-03 20:58:14 +0200

    Upgraded to .NET Framework  4.8

--- Validator/Properties/AssemblyInfo.cs
+++ Validator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Validator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET Input Validator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Validator")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
+[assembly: AssemblyCopyright("Copyright ©  AS 2009 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
